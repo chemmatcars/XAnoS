Fixed a bug in Sphere_Double_Layer regarding the calculation of ion distribution in the bulk. Added Fitting Information in the XAnoS_Fit.py Added multiple color options for Data_Dialog.py

Functions/ASAXS/Sphere_Double_Layer.py
@@ -269,7 +269,7 @@ class Sphere_Double_Layer: #Please put the class name same as the function name
                 solvent_mw = self.__cf__.molecular_weight()
                 solvent_mole_ratio = self.__cf__.element_mole_ratio()
 
-                solvent_moles = sol_density[i] / solvent_mw
+                solvent_moles = sol_density[i]*(1-solute_mv*density[i]/solute_mw)/ solvent_mw
                 solute_moles = density[i] / solute_mw
                 total_moles = solvent_moles + solute_moles
                 solvent_mole_fraction = solvent_moles / total_moles
@@ -309,13 +309,13 @@ class Sphere_Double_Layer: #Please put the class name same as the function name
                     fmratio = mole_ratio[self.farIon]
                 tfar=tfar+(r2**3-r1**3)*tdensity*1e3*fmratio/molwt#mwfion
             r1=r2+0.0
-        integral=(R2**2*lexp+2*R2*lexp**2+2*lexp**3)*np.exp(-R2/lexp)-(Rp**2*lexp+2*Rp*lexp**2+2*lexp**3)*np.exp(-Rp/lexp)
+        integral=(R2**2*lexp+2*R2*lexp**2+2*lexp**3)-(Rp**2*lexp+2*Rp*lexp**2+2*lexp**3)*np.exp((R2-Rp)/lexp)
         if 3*lrho*integral>=rhosol*(Rp**3-R1**3)-strho*(R2**3-R1**3)-tnear:
             near=0.00
             lrho=(rhosol*(Rp**3-R1**3)-strho*(R2**3-R1**3)-tnear)/3/integral
         else:
-            near=(rhosol*(Rp**3-R1**3)-strho*(R2**3-R1**3)-3*lrho*integral-tnear)/(Rp**3-R2**3)
-        far=(rhosol*(Rp ** 3 - R1 ** 3)+3*lrho*integral-tfar)/(Rp**3 - R2**3)
+            near=(rhosol*(Rp**3-R1**3)-strho*(R2**3-R1**3)-3*lrho*integral-tnear)/(Rp**3-R2**3-3*integral)
+        far=(rhosol*(Rp ** 3 - R1 ** 3)-tfar)/(Rp **3-R2**3 -3*lrho*integral)
         self.output_params['scaler_parameters']['tnear']=tnear
         self.output_params['scaler_parameters']['tfar']=tfar
         self.output_params['scaler_parameters']['rho_near']=near

XAnoS_Fit.py
@@ -648,7 +648,8 @@ class XAnoS_Fit(QWidget):
                         if key not in self.fit.fit_params.keys() and key not in self.special_keys and key[:2]!='__':
                             header+=key+'='+str(self.fit.params[key])+'\n'
                     header+=self.fit_report+'\n'
-                    header+='x \t y\t yerr \t yfit'
+                    header+="col_names=['x','y','yerr','yfit']\n"
+                    header+='x \t y\t yerr \t yfit\n'
                     if type(self.fit.x)==dict:
                         for key in self.fit.x.keys():
                             fitdata=np.vstack((self.fit.x[key][self.fit.imin[key]:self.fit.imax[key]+1],self.fit.y[key][self.fit.imin[key]:self.fit.imax[key]+1],\
@@ -930,20 +931,25 @@ class XAnoS_Fit(QWidget):
         self.loadParamButton=QPushButton('Load Parameters')
         self.loadParamButton.clicked.connect(lambda x: self.loadParameters(fname=None))
         self.mfitparamLayoutWidget.addWidget(self.loadParamButton,col=2)
-        
-        self.parSplitter.addWidget(self.mfitparamLayoutWidget)       
+        self.parSplitter.addWidget(self.mfitparamLayoutWidget)
+
+        self.fitResultsLayoutWidget=pg.LayoutWidget()
+        fitResults=QLabel('Fit Results')
+        self.fitResultsLayoutWidget.addWidget(fitResults,colspan=1)
+        self.fitResultsLayoutWidget.nextRow()
+        self.fitResultsListWidget=QListWidget()
+        self.fitResultsLayoutWidget.addWidget(self.fitResultsListWidget,colspan=1)
+        self.parSplitter.addWidget(self.fitResultsLayoutWidget)
         
         self.genparamLayoutWidget=pg.LayoutWidget()
         genParameters=QLabel('Generated Parameters')
         self.genparamLayoutWidget.addWidget(genParameters,colspan=2)
-        
         self.genparamLayoutWidget.nextRow()
         self.genParamListWidget=QListWidget()
         self.genParamListWidget.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.genParamListWidget.itemSelectionChanged.connect(self.plot_extra_param)
         #self.genParamListWidget.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
         self.genparamLayoutWidget.addWidget(self.genParamListWidget,colspan=2)
-        
         self.genparamLayoutWidget.nextRow()
         self.saveGenParamButton=QPushButton('Save Generated Parameters')
         self.saveGenParamButton.clicked.connect(self.saveGenParameters)
@@ -1720,6 +1726,13 @@ class XAnoS_Fit(QWidget):
                 self.genParamListWidget.itemSelectionChanged.disconnect(self.plot_extra_param)
             except:
                 pass
+            self.fitResultsListWidget.clear()
+            try:
+                self.fitResultsListWidget.addItem('Chi-Square : %f' % self.fit.result.chisqr)
+                self.fitResultsListWidget.addItem('Reduced Chi-Square : %f' % self.fit.result.redchi)
+                self.fitResultsListWidget.addItem('Fit Message : %s' % self.fit.result.lmdif_message)
+            except:
+                self.fitResultsListWidget.clear()
             self.genParamListWidget.clear()
             if len(self.fit.params['output_params'])>0:
                 for key in self.fit.params['output_params'].keys():